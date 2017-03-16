e023b2edc2cd841cef0b993dbfa3066b25b42b84
Chris Scott <christocracy@gmail.com>
2017-03-16 09:48:53 -0400

    Destroy old ionic1-based cordova app in preparation for new Ionic2 code

--- script/build-ios.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo "- rebuilding"
-cordova plugin remove com.transistorsoft.cordova.background-geolocation && cordova plugin add ../cordova-background-geolocation && cordova build ios
-
-
-
