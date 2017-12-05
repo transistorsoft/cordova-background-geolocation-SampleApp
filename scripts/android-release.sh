b82008fa4b6084f2daa2f348ecfc1440da21d05c
Chris Scott <christocracy@gmail.com>
2017-12-05 15:48:40 -0500

    Update release script, .gitignore

--- scripts/android-release.sh
+++ scripts/android-release.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-ionic cordova build android --prod --release
+ionic cordova build android --release --prod
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/Dropbox/TransistorSoftware/Apps/com.transistorsoft.backgroundgeolocation.ionic2/release-key.keystore platforms/android/build/outputs/apk/android-release-unsigned.apk com.transistorsoft.backgroundgeolocation.ionic2
 ~/Library/Android/sdk/build-tools/26.0.1/zipalign  -v 4 platforms/android/build/outputs/apk/android-release-unsigned.apk ./cordova-background-geolocation.apk
